91484c28b50d31b6fcd3083fd07a01dcaf9b3dd8
iam-vasant <vk.vasanth.r@gmail.com>
2026-01-04 00:16:13 +0530

    Added symlinks.nix

--- home/symlinks.nix
+++ home/symlinks.nix
@@ -1,5 +1,5 @@
 {config, ...}:
 
 {
-  home.file.".config/noctalia/config.kdl".source = ./configs/niri/config.kdl;
+  home.file.".config/noctalia/config.kdl".source = ./home/configs/niri/config.kdl;
 }
